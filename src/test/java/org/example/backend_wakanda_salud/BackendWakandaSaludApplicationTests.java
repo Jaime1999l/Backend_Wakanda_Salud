190f12d2bb509a5a4a6f9c1bf3da001643997513
Jaime Lopez Diaz <jlopedia@myuax.com>
2024-11-26 13:31:42 +0100

    Initial commit

---
+++ src/test/java/org/example/backend_wakanda_salud/BackendWakandaSaludApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backend_wakanda_salud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWakandaSaludApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

702f2950a39d7edba1fb486aae10ba07744f204d
marcos <marcgabe@myuax.com>
2024-12-04 10:58:52 +0100

    CHECKPOINT

--- src/main/java/org/example/backend_wakanda_salud/BackendWakandaSaludApplication.java
+++ src/main/java/org/example/backend_wakanda_salud/BackendWakandaSaludApplication.java
@@ -6,11 +6,13 @@ import org.example.backend_wakanda_salud.service.citas.SistemaCitasService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import java.util.List;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BackendWakandaSaludApplication {
 
     public static void main(String[] args) {
